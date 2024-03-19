a38bc03dd18e2c1d0e8f42940be029ef75f9e1ba
root <RobinLeeHenry101@gmail.com>
2024-03-19 13:32:38 +0000

    testing task 0

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -5,12 +5,17 @@ import random
 
 async def async_generator():
     """ loop 10 times"""
+    result = []
     for _ in range(10):
         await asyncio.sleep(1)
-        yield random.uniform(0, 10)
+        num = random.uniform(0, 10)
+        result.append(num)
+        yield num
+
+    print(result)
 
 async def main():
     async for num in async_generator():
-        print(num)
+        pass
 
 asyncio.run(main())
