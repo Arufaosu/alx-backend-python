87467a5125b6f4d7da8ee79fce4d4088364864bb
root <RobinLeeHenry101@gmail.com>
2024-04-08 08:15:52 +0000

    implemented a test_public_repo method

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -19,9 +19,9 @@ class TestIntegrationGithubOrgClient(unittest.TestCase):
 
         cls.mock_get = cls.get_patcher.start()
         cls.mock_get.side_effect = [
-            org_payload,  # Mock the response for org payload
-            repos_payload,  # Mock the response for repos payload
-            repos_payload,  # Mock the response for repos payload for Apache2 license check
+            cls.org_payload,  # Mock the response for org payload
+            cls.repos_payload,  # Mock the response for repos payload
+            cls.repos_payload,  # Mock the response for repos payload for Apache2 license check
         ]
 
     @classmethod
@@ -36,21 +36,22 @@ class TestIntegrationGithubOrgClient(unittest.TestCase):
         expected_repos_names = [repo['name'] for repo in self.expected_repos]
 
         org_client = GithubOrgClient(org_name)
-        res1 = org_client.public_repos()
-        res2 = org_client.public_repos()
+        result = org_client.public_repos()
 
         self.mock_get.assert_called_with(f'https://api.github.com/orgs/{org_name}/repos')
-        self.assertEqual(res1, expected_repos_names)
-        self.assertEqual(res2, expected_repos_names)
+        self.assertEqual(result, expected_repos_names)
 
-    def test_has_license_apache2(self):
-        """Test has_license method for Apache2 license"""
+    def test_public_repos_with_license(self):
+        """Test public_repos method with license"""
 
         org_name = self.org_payload['login']
-        org_client = GithubOrgClient(org_name)
+        expected_repos_names = [repo['name'] for repo in self.apache2_repos]
 
-        for repo in self.apache2_repos:
-            self.assertTrue(org_client.has_license(repo, 'apache-2.0'))
+        org_client = GithubOrgClient(org_name)
+        result = org_client.public_repos(license="apache-2.0")
+
+        self.mock_get.assert_called_with(f'https://api.github.com/orgs/{org_name}/repos')
+        self.assertEqual(result, expected_repos_names)
 
 
 if __name__ == '__main__':
